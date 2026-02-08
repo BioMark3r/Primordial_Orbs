250fad96660c2a8b5c7a56fb93d13887ae7ff86a
BioMark3r <121187458+BioMark3r@users.noreply.github.com>
2026-02-08 13:47:13 -0500

    Add Cataclysm Arena UI panel

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./ui/theme/global.css";
+import "./ui/theme/arena.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
